3a5b2800f8f9c28931550c36b23b5c43ecabc70a
kalilpock <kalilpock54@gmail.com>
2025-09-19 10:57:15 -0300

    initAula

---
+++ 2DSB/PokedexJS/script.js
@@ -0,0 +1,4 @@
+const buscaInput = document.getElementById("busca-input");
+const buscaBotao = document.getElementById("busca-botao");
+
+const pokemonCards = document.querySelectorAll(".pokemon-card")
