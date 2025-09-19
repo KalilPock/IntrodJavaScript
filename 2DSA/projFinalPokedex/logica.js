3a9ce4e2e06512efd84fd3fb2b63e168bd579b4c
kalilpock <kalilpock54@gmail.com>
2025-09-19 12:08:06 -0300

    cod Final Aula

---
+++ 2DSA/projFinalPokedex/logica.js
@@ -0,0 +1,8 @@
+const pesquisaInput = document.getElementById("pesquisa-input")
+const pesquisaBotao = document.getElementById("pesquisa-botao")
+
+const pokemonCard = document.querySelectorAll(".card")
+
+pesquisaBotao.addEventListener('click', ()=>{
+    const filtroPesquisa = filtroPesquisa.value.toLoweCase();
+})
