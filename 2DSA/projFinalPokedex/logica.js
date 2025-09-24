6d94d003f1ad4ff5c1c61dfa596dcc5ce814f443
kalilpock <kalilpock54@gmail.com>
2025-09-24 09:44:14 -0300

    logicaDePesquisa

--- 2DSA/projFinalPokedex/logica.js
+++ 2DSA/projFinalPokedex/logica.js
@@ -1,8 +1,23 @@
+//Selecionando os elementos HTML
 const pesquisaInput = document.getElementById("pesquisa-input")
 const pesquisaBotao = document.getElementById("pesquisa-botao")
-
 const pokemonCard = document.querySelectorAll(".card")
 
+//"Ouvindo" evento do botao
 pesquisaBotao.addEventListener('click', ()=>{
-    const filtroPesquisa = filtroPesquisa.value.toLoweCase();
-})
+    const filtroPesquisa = pesquisaInput.value.toLowerCase();
+
+    //percorrendo os cards
+    pokemonCard.forEach(card => {
+        const pokemonNome = card.getAttribute('data-name').toLowerCase();
+
+        //logica de filtragem
+        if(pokemonNome.includes(filtroPesquisa)){
+            card.classList.remove('d-none');
+        } else {
+            card.classList.add('d-none');
+        }
+    });
+});
+
+
