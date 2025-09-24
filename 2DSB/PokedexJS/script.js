aaca55b74df47c44567e0eebb8185eb1bd172ddd
kalilpock <kalilpock54@gmail.com>
2025-09-24 11:49:56 -0300

    FinalAula

--- 2DSB/PokedexJS/script.js
+++ 2DSB/PokedexJS/script.js
@@ -1,4 +1,31 @@
-const buscaInput = document.getElementById("busca-input");
-const buscaBotao = document.getElementById("busca-botao");
+//Selecionando os elementos HTML
+const pesquisaInput = document.getElementById("pesquisa-input");
+const pesquisaBotao = document.getElementById("pesquisa-botao");
+const pokemonCard = document.querySelectorAll(".card");
 
-const pokemonCards = document.querySelectorAll(".pokemon-card")
+//impedir que a pagina regarregue
+const pesquisaForm = document.getElementById('pesquisa-form');
+
+//"Ouvinte" para o formulario não atualizar a pagina
+pesquisaForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+});
+
+//"Ouvindo" evento do botao 
+pesquisaBotao.addEventListener('click', ()=>{
+    const filtroPesquisa = pesquisaInput.value.toLowerCase();
+
+    //percorrendo os cards
+    pokemonCard.forEach(card => {
+        const pokemonNome = card.getAttribute('data-name').toLowerCase();
+
+        //logica de filtragem
+        if(pokemonNome.includes(filtroPesquisa)){
+            card.classList.remove('d-none');
+        } else {
+            card.classList.add('d-none');
+        }
+    });
+});
+
+
