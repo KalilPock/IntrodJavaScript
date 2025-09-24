51bc8f2afa71c75269cfff8f6e79dfbb4e3848ff
kalilpock <kalilpock54@gmail.com>
2025-09-24 10:09:04 -0300

    logicaFinalizada

--- 2DSA/projFinalPokedex/logica.js
+++ 2DSA/projFinalPokedex/logica.js
@@ -1,9 +1,17 @@
 //Selecionando os elementos HTML
-const pesquisaInput = document.getElementById("pesquisa-input")
-const pesquisaBotao = document.getElementById("pesquisa-botao")
-const pokemonCard = document.querySelectorAll(".card")
+const pesquisaInput = document.getElementById("pesquisa-input");
+const pesquisaBotao = document.getElementById("pesquisa-botao");
+const pokemonCard = document.querySelectorAll(".card");
 
-//"Ouvindo" evento do botao
+//impedir que a pagina regarregue
+const pesquisaForm = document.getElementById('pesquisa-form');
+
+//"Ouvinte" para o formulario não atualizar a pagina
+pesquisaForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+});
+
+//"Ouvindo" evento do botao 
 pesquisaBotao.addEventListener('click', ()=>{
     const filtroPesquisa = pesquisaInput.value.toLowerCase();
 
